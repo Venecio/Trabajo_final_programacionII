89612d915f57d3ea322a5554eb444297bf0e0138
Venecio <Cristianpessio11@gmail.com>
2021-07-29 15:38:27 -0300

    Todo funciona

--- carrito/carrito.php
+++ carrito/carrito.php
@@ -5,21 +5,34 @@ session_start();
 $id_user = $_SESSION['id_user'];
 $consulta = "SELECT * FROM compras WHERE id_user = '$id_user' and estado_compra = 0";
 $resultado = mysqli_query($conexion, $consulta);
-$row = mysqli_num_rows($resultado);
-$valores_consulta = mysqli_fetch_assoc($resultado);
-$num_compra = $valores_consulta['numero_compra'];
+$cantidad_productos_carrito = mysqli_num_rows($resultado);
 
-if ($row > 0) {
+if ($cantidad_productos_carrito > 0) {
+
+    $productos_carrito = mysqli_fetch_assoc($resultado);
+    $num_compra = $productos_carrito['numero_compra'];
+
+
     $consulta = "INSERT INTO compras (id_user,id_producto,estado_compra,numero_compra) VALUES ($id_user,$id_producto,0,$num_compra)";
     $resultado = mysqli_query($conexion, $consulta);
 } else {
-    $numero = "SELECT MAX(numero_compra) FROM compras";
+
+    $consulta = "SELECT MAX(numero_compra) as numero FROM compras";
+    $resultado = mysqli_query($conexion, $consulta);
+
+    $row = mysqli_fetch_assoc($resultado);
+    $numero = (int)$row['numero'];
+
+    echo $numero;
     if ($numero >= 0) {
         $numero = $numero + 1;
     }
+
+
     $consulta = "INSERT INTO compras (id_user,id_producto,estado_compra,numero_compra) VALUES ($id_user,$id_producto,0,$numero)";
     $resultado = mysqli_query($conexion, $consulta);
 }
+
 
 if ($_GET['eleccion'] == 'Hamburguesa') {
     header("Location: ../productos/verproductos.php?eleccion=Hamburguesa");
